f3ff042eb4cdf55ad8aa41eb5173f23acdbf0325
Pockethouse <101947734+Pockethouse@users.noreply.github.com>
2022-09-28 11:10:26 -0400

    Used custom cells for UI

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Mark Bowen on 9/27/22.
+//
+
+import Foundation
+
+
+class SearchResult {
+
+    
+    var name = ""
+  var artistName = ""
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
