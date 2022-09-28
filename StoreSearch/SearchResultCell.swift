961869822389ae4390ca66fef21c99c230f8acbc
Pockethouse <101947734+Pockethouse@users.noreply.github.com>
2022-09-28 15:14:43 -0400

    version 2.0

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -8,26 +8,19 @@
 import UIKit
 
 class SearchResultCell: UITableViewCell {
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var artistNameLabel: UILabel!
+  @IBOutlet var artworkImageView: UIImageView!
 
-    //Outlets
-    @IBOutlet weak var nameLabel: UILabel!
-   @IBOutlet weak var artistNameLabel: UILabel!
-   @IBOutlet weak var artworkImageView: UIImageView!
-    
-    
-        
-        override func awakeFromNib() {
-          super.awakeFromNib()
-          // New code below
-          let selectedView = UIView(frame: CGRect.zero)
-          selectedView.backgroundColor = UIColor(named: "SearchBar")?.withAlphaComponent(0.5)
-            selectedBackgroundView = selectedView
-          }
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    let selectedView = UIView(frame: CGRect.zero)
+    selectedView.backgroundColor = UIColor(named: "SearchBar")?.withAlphaComponent(0.5)
+    selectedBackgroundView = selectedView
+  }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+  }
 }
